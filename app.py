c5e152be98ce59852eae076afdf8ee2803d2163e
Henrik Hellerud <hellerudhenrik@gmail.com>
2022-03-28 18:03:59 +0200

    added comments

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 #creating a flask instance
 app = Flask(__name__)
 
-#creating a route/decorator
+#creating a route/decorator to home page
 @app.route('/')
 def home():
     pizza = ['margherita', 'pepperoni', 'deluxe', 'meat tornado', 'vegetarian']
@@ -11,18 +11,19 @@ def home():
         pizza = pizza
         )
 
-
+#routing to users
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', 
         user_name = name
         )
 
-#invalid url
+#invalid url handling
 @app.errorhandler(404)
 def pagenotfount(e):
     return render_template('404.html'), 404
 
+#server error handling
 @app.errorhandler(500)
 def pagenotfount(e):
     return render_template('500.html'), 500
